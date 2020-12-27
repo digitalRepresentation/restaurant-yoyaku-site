ec46df7c949ce1f736307add9d1ae2e03c4919a3
digitalRepresentation <kinemura@naver.com>
2020-12-28 00:06:11 +0900

    最初画面表示テスト

---
+++ restaurant-api/src/main/java/jp/co/jinyoung/restaurant/Interface/WelcomeController.java
@@ -0,0 +1,13 @@
+package jp.co.jinyoung.restaurant.Interface;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
