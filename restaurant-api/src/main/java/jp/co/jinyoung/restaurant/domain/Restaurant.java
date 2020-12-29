72a974b55663210166cb982069e0f589fca85742
digitalRepresentation <kinemura@naver.com>
2020-12-30 00:44:19 +0900

    creation, information メソッド追加、テストコード追加

--- restaurant-api/src/main/java/jp/co/jinyoung/restaurant/domain/Restaurant.java
+++ restaurant-api/src/main/java/jp/co/jinyoung/restaurant/domain/Restaurant.java
@@ -1,4 +1,24 @@
 package jp.co.jinyoung.restaurant.domain;
 
 public class Restaurant {
+    private final String name;
+    private final String address;
+
+    public Restaurant(String name, String address) {
+
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInforamation() {
+        return name + " in " + address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
--- restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantTests.java
+++ restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantTests.java
@@ -2,12 +2,21 @@ package jp.co.jinyoung.restaurant.domain;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class RestaurantTests {
 
     @Test
     public void creation() {
-        Restaurant restaurant = new Restaurant();
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getInforamation(), is("Bob zip in Seoul"));
     }
 }
