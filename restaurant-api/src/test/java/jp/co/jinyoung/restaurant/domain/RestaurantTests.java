0d06970f7611483945c8945e1d133bd0ee4c62d4
digitalRepresentation <kinemura@naver.com>
2020-12-29 13:48:35 +0900

    junit4 追加

---
+++ restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantTests.java
@@ -0,0 +1,9 @@
+package jp.co.jinyoung.restaurant.domain;
+
+import static org.junit.Assert.*;
+
+public class RestaurantTests {
+    public void creation() {
+        Restaurant restaurant = new Restaurant();
+    }
+}
