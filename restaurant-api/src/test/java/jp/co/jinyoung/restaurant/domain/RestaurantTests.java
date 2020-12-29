2b25cebf48ea073fa5204e43231885c8ead0e9c5
digitalRepresentation <kinemura@naver.com>
2020-12-30 00:26:19 +0900

    junit4 testcodeできるように設定

--- restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantTests.java
+++ restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantTests.java
@@ -1,8 +1,12 @@
 package jp.co.jinyoung.restaurant.domain;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RestaurantTests {
+
+    @Test
     public void creation() {
         Restaurant restaurant = new Restaurant();
     }
