d7f39a203a6543ee61f1019d2c126b2f523fe7f6
digitalRepresentation <kinemura@naver.com>
2020-12-31 00:51:50 +0900

    レストランお店リスト初期ファイル作成（テストコード含み）

---
+++ restaurant-api/src/main/java/jp/co/jinyoung/restaurant/Interface/RestaurantController.java
@@ -0,0 +1,4 @@
+package jp.co.jinyoung.restaurant.Interface;
+
+public class RestaurantController {
+}
---
+++ restaurant-api/src/test/java/jp/co/jinyoung/restaurant/Interface/RestaurantControllerTests.java
@@ -0,0 +1,7 @@
+package jp.co.jinyoung.restaurant.Interface;
+
+import static org.junit.Assert.*;
+
+public class RestaurantControllerTests {
+
+}
