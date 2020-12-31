beadfa7ade7aa6b67f457bd063508575abd3aabc
digitalRepresentation <kinemura@naver.com>
2020-12-31 14:59:32 +0900

    レストランのID情報追加（レストラン店舗検索）

--- restaurant-api/src/main/java/jp/co/jinyoung/restaurant/Interface/RestaurantController.java
+++ restaurant-api/src/main/java/jp/co/jinyoung/restaurant/Interface/RestaurantController.java
@@ -1,4 +1,23 @@
 package jp.co.jinyoung.restaurant.Interface;
 
+
+import jp.co.jinyoung.restaurant.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list() {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Chiba");
+
+        restaurants.add(restaurant);
+        return restaurants;
+    }
 }
