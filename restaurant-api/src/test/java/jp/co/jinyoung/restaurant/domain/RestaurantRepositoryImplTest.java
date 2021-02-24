73ab4fa9f87dea2bce9a45e25d856f070b887122
digitalRepresentation <kinemura@naver.com>
2021-02-24 20:29:18 +0900

    RepositoryTest追加

---
+++ restaurant-api/src/test/java/jp/co/jinyoung/restaurant/domain/RestaurantRepositoryImplTest.java
@@ -0,0 +1,26 @@
+package jp.co.jinyoung.restaurant.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RestaurantRepositoryImplTest {
+
+    @Test
+    public void save() {
+        RestaurantRepository repository = new RestaurantRepositoryImpl();
+
+        int oldCount = repository.findAll().size();
+
+        Restaurant restaurant = new Restaurant("Samgyopsal", "matsudo");
+        repository.save(restaurant);
+
+        assertThat(restaurant.getId(), is(1234L));
+
+        int newCount = repository.findAll().size();
+
+        assertThat(newCount - oldCount, is(1));
+    }
+
+}
